f47302f0496269c2c4e1d27fd7199ca767870d63
Cory Sebastian <44050949+corith@users.noreply.github.com>
2020-06-29 14:17:17 -0500

    Update EditJobRequest.php

--- app/Http/Requests/Marketplace/EditJobRequest.php
+++ app/Http/Requests/Marketplace/EditJobRequest.php
@@ -28,8 +28,8 @@ class EditJobRequest extends FormRequest
         return [
             'description' => [ 'string'],
             'complete_before' => ['date'],
-            'intensity_id' => ['unsignedBigInteger'],
-            'price' => ['double'],
+            'intensity_id' => ['required', 'exists:job_intensities,id'],
+            'price' => ['numeric'],
             'street_address' => [ 'string'],
             'city' => [ 'string'],
             'state' => [ new Abbr('US')],
