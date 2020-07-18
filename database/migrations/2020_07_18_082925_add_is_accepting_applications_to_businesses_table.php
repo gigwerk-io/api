88d6171b8e267944b5f7af128a713b63760aafaa
Cory Sebastian <sebastiancory@yahoo.com>
2020-07-18 12:55:11 -0500

    feat(dragon): 🎸 Fixed a migration issue
    BREAKING CHANGE: 🧨 no
    
    ✅ Closes: GIGWERK-151

--- database/migrations/2020_07_18_082925_add_is_accepting_applications_to_businesses_table.php
+++ database/migrations/2020_07_18_082925_add_is_accepting_applications_to_businesses_table.php
@@ -14,7 +14,7 @@ class AddIsAcceptingApplicationsToBusinessesTable extends Migration
     public function up()
     {
         Schema::table('businesses', function (Blueprint $table) {
-            $table->boolean('is_accepting_applications')->after('name');
+            $table->boolean('is_accepting_applications')->after('name')->default(0);
         });
     }
 
@@ -26,7 +26,7 @@ class AddIsAcceptingApplicationsToBusinessesTable extends Migration
     public function down()
     {
         Schema::table('businesses', function (Blueprint $table) {
-            //
+            $table->dropColumn('is_accepting_applications');
         });
     }
 }
