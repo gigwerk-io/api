217ea3dfa3d433033d6c8281c0c262f17bde7ea9
Cory Sebastian <sebastiancory@yahoo.com>
2020-07-19 21:44:43 -0500

    feat(dragon): 🎸 Added extra fields to business table
    added facebook_pixel_id & google_analytics_id & cloudfront_id &
    s3_bucket_id to businesses table. I updated the fillable array in the
    business model and updated the business factory.
    
    BREAKING CHANGE: 🧨 nop
    
    ✅ Closes: GIGWERK-161

--- database/factories/BusinessFactory.php
+++ database/factories/BusinessFactory.php
@@ -12,5 +12,9 @@ $factory->define(Business::class, function (Faker $faker) {
         'subdomain_prefix' => $faker->domainWord,
         'stripe_connect_id' => 'acct_1F7RiLBKeAbZ6utM',
         'is_accepting_applications' => 1,
+        'facebook_pixel_id' => null,
+        'google_analytics_id' => null,
+        'cloudfront_id' => null,
+        's3_bucket_id' => null,
     ];
 });
