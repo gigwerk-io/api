a24e78fee9806fe733e1a3256d042881153665d7
Cory Sebastian <sebastiancory@yahoo.com>
2020-07-22 15:07:23 -0500

    feat(dragon): 🎸 Added docs for toDatabase()
    Added documetation for toDatabase()
    
    BREAKING CHANGE: 🧨 no
    
    ✅ Closes: GIGWERK-169

--- app/Notifications/User/ApplicationApprovedNotification.php
+++ app/Notifications/User/ApplicationApprovedNotification.php
@@ -64,6 +64,12 @@ class ApplicationApprovedNotification extends Notification
                     ->line('Thank you for using our application!');
     }
 
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
     public function toDatabase($notifiable)
     {
         return [
