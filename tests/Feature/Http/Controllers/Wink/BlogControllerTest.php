719c78b30b45d0f1b57c7f2a23c528cc2576187e
vagrant <vagrant@gigwerk>
2020-08-09 22:36:05 +0000

    fix(website): 🐛 fixed unit test
    BREAKING CHANGE: 🧨 no
    
    ✅ Closes: GIGWERK-178

---
+++ tests/Feature/Http/Controllers/Wink/BlogControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Wink;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+use Illuminate\Database\DatabaseManager;
+use Tests\ResponseFactoryTest;
+
+/**
+ * @coversDefaultClass \App\Http\Controllers\Wink\BlogController
+ */
+class BlogControllerTest extends TestCase
+{
+    const DOC_PATH = 'wink/blog-posts';
+    const ALL_BLOG_POSTS_ROUTE = 'all.blog.posts';
+    const SHOW_BLOG_POST_ROUTE = 'show.blog.post';
+
+    /**
+     * @covers ::index
+     */
+    public function testViewAllBlogPosts()
+    {
+        $response = $this->get(route(self::ALL_BLOG_POSTS_ROUTE));
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['data' => 'data', 'message', 'success']);
+        $this->document(self::DOC_PATH, self::ALL_BLOG_POSTS_ROUTE, $response->status(), $response->getContent());
+    }
+
+    public function testViewBlogPost()
+    {
+        $response = $this->get(route(self::SHOW_BLOG_POST_ROUTE, ['id' => '6342be30-9704-4f51-9f8c-929e4f990ce6']));
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['data' => 'data', 'message', 'success']);
+        $this->document(self::DOC_PATH, self::SHOW_BLOG_POST_ROUTE, $response->status(), $response->getContent());
+    }
+}
