04768017e9339c63622912dbb558f5736c3b715b
Zion Comer <zioncomer@gmail.com>
2020-09-05 15:52:09 -0700

    feat(dragon): 🎸 Added to form controller
    BREAKING CHANGE: 🧨 no
    
    ✅ Closes: GIGWERK-203

--- app/Http/Controllers/Business/DragDropFormController.php
+++ app/Http/Controllers/Business/DragDropFormController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers\Business;
 
+use App\Factories\ResponseFactory;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class DragDropFormController extends Controller
 {
     public function store(Request $request)
     {
+        $data = $request->all();
+
+        return ResponseFactory::success('form data submitted', $data);
 
     }
 }
